470f8b3447be551fed46d31f60f030f7761f3db7
jinu <jinwoo794533@gmail.com>
2022-01-05 19:05:29 +0900

    :sparkles: Email Service

--- src/main/kotlin/com/appsolulte/smupbackendserver/global/configuration/GlobalProperties.kt
+++ src/main/kotlin/com/appsolulte/smupbackendserver/global/configuration/GlobalProperties.kt
@@ -6,11 +6,16 @@ import org.springframework.boot.context.properties.ConstructorBinding
 @ConstructorBinding
 @ConfigurationProperties(prefix = "defined")
 class GlobalProperties (
-    val fcm: FcmProperties
+    val fcm: FcmProperties,
+    val email: EmailProperties
 ){
     data class FcmProperties(
         var url: String,
         var firebaseConfigPath: String
+    )
+    data class EmailProperties(
+        var account: String,
+        var password: String
     )
 
 
