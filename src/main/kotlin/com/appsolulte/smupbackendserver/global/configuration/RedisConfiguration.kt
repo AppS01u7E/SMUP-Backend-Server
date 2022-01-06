4e60a4b18ab92b48fc2d25f4b9248189070b288c
jinu <jinwoo794533@gmail.com>
2022-01-06 21:05:06 +0900

    :sparkles: redis COnfig

---
+++ src/main/kotlin/com/appsolulte/smupbackendserver/global/configuration/RedisConfiguration.kt
@@ -0,0 +1,21 @@
+package com.appsolulte.smupbackendserver.global.configuration
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+
+@Configuration
+class RedisConfiguration {
+    @Value("${spring.redis.host}")
+    private val host: String? = null
+
+    @Value("${spring.redis.port}")
+    private val port = 0
+
+    @Bean
+    fun redisConnectionFactory(): RedisConnectionFactory? {
+        return LettuceConnectionFactory(host!!, port)
+    }
+}
