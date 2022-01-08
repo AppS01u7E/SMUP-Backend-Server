6f3319fce5f22df400df975635f2536af6fb67f4
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-01-08 19:40:50 +0900

    ♻️ 전략패턴을 통한 관심사 분리

--- src/main/java/com/appsolute/soomapi/domain/account/service/RedisTeacherAuthorizeService.java
+++ src/main/java/com/appsolute/soomapi/domain/account/service/RedisTeacherAuthorizeService.java
@@ -1,5 +1,6 @@
 package com.appsolute.soomapi.domain.account.service;
 
+import com.appsolute.soomapi.domain.account.strategy.TeacherSignupCodeExpiredStrategy;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
@@ -20,20 +21,16 @@ import java.util.stream.IntStream;
 public class RedisTeacherAuthorizeService implements TeacherAuthorizeService{
     private static final String KEY_PREFIX = "teacher-signup-code"; //다른 용도로 redis 가 쓰일 수 있으므로, prefix 로 key 값을 구분한다.
     private final RedisTemplate<String, String> redisTemplate;
+    private final TeacherSignupCodeExpiredStrategy expiredStrategy;
 
     private final Random random = new Random();
 
     @Override
     public List<String> generateTeacherCode(Integer quantity) {
-        //TODO [지인호] DateTime 관련 내용을 TeacherSignupCodeExpiredStrategy 로 분리하기
-        LocalDateTime datetime = LocalDateTime.now().plusDays(7);//토큰은 일주일간 유효하다
-        long expiredAt = datetime.toLocalDate()
-                .atStartOfDay(ZoneId.of("Asia/Seoul"))
-                .toInstant()
-                .toEpochMilli(); //일주일 후 0시 0분의 시각을 밀리초로 변환한다.
+        long expiredAt = expiredStrategy.getExpiredDateToLong(LocalDateTime.now()); //해당 코드의 유효기간을 구한다.
         return IntStream.range(0, quantity).mapToObj(i -> {
             String code = generateTeacherCode();
-            redisTemplate.opsForValue().set(KEY_PREFIX + code, String.valueOf(expiredAt));
+            redisTemplate.opsForValue().set(KEY_PREFIX + code, String.valueOf(expiredAt)); //유효기간을 value 로 하는 코드를 저장한다 (사실 value 에 쓸게 없어요)
             return code;
         }).collect(Collectors.toList());
     }
