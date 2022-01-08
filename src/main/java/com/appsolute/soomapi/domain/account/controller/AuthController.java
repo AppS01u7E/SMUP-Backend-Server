e6dc65e9f4468292a9520f932e3ce48acb0f08af
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-01-08 16:39:12 +0900

    🐛 Controller 구성 관련 오류 수정

--- src/main/java/com/appsolute/soomapi/domain/account/controller/AuthController.java
+++ src/main/java/com/appsolute/soomapi/domain/account/controller/AuthController.java
@@ -1,8 +1,9 @@
 package com.appsolute.soomapi.domain.account.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 //계정 인증 관리 - 인증 API
-@RestController("/api/v1/account/auth")
+@RestController
+@RequestMapping("/api/v1/account/auth")
 public class AuthController {
 }
--- src/main/java/com/appsolute/soomapi/domain/account/controller/LoginController.java
+++ src/main/java/com/appsolute/soomapi/domain/account/controller/LoginController.java
@@ -1,8 +1,10 @@
 package com.appsolute.soomapi.domain.account.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 //계정 인증 관리 - 로그인/아웃 API
-@RestController("/api/v1/account/signin")
+@RestController
+@RequestMapping("/api/v1/account/signin")
 public class LoginController {
 }
