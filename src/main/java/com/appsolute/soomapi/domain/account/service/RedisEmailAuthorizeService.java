ff528dbfe0fa2f68e862ac307c699aef533b4e9e
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-01-09 17:15:23 +0900

    🎨 MailSenderService 를 RedisEmailAuthorizeService 에 적용

--- src/main/java/com/appsolute/soomapi/domain/account/service/RedisEmailAuthorizeService.java
+++ src/main/java/com/appsolute/soomapi/domain/account/service/RedisEmailAuthorizeService.java
@@ -1,19 +1,17 @@
 package com.appsolute.soomapi.domain.account.service;
 
+import com.appsolute.soomapi.infra.service.MailSenderService;
 import io.jsonwebtoken.Header;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.RequiredArgsConstructor;
 import org.joda.time.LocalDateTime;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.context.Context;
 
-import javax.mail.MessagingException;
-import javax.mail.internet.MimeMessage;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 //Redis 로 인증 정보를 저장하는 이메일 인증 서비스
@@ -22,7 +20,7 @@ import java.util.Random;
 public class RedisEmailAuthorizeService implements EmailAuthorizeService {
     private static final String KEY_PREFIX = "email-authorize-code"; //다른 용도로 redis 가 쓰일 수 있으므로, prefix 로 key 값을 구분한다.
     private final RedisTemplate<String, String> redisTemplate;
-    private final JavaMailSender jms; //TODO [지인호] MailSenderService 작성하기
+    private final MailSenderService mailSenderService;
     private final TemplateEngine templateEngine;
 
     private final Random random = new Random();
@@ -47,23 +45,12 @@ public class RedisEmailAuthorizeService implements EmailAuthorizeService {
         return redisTemplate.opsForValue().get(KEY_PREFIX + code);
     }
 
-    @Override //TODO [지인호] 나중에 MailSenderService 를 통해 관심사 분리 예정
+    private static final String AUTHORIZE_MAIL_TITLE = "[SOOM] 인증번호가 도착했어요!";
+    @Override
     public void sendAuthorizeEmail(String code, String email) {
-        MimeMessage message = jms.createMimeMessage();
-
-        Context context = new Context();
-        context.setVariable("code", code);
-        String content = templateEngine.process("authorize/mail", context);
-        try  {
-            MimeMessageHelper helper = new MimeMessageHelper(message, true);
-            helper.setSubject("[SOOM] 인증번호가 도착했어요!");
-            helper.setTo(email);
-            helper.setText(content, true);
-        } catch (MessagingException e) {
-            throw new RuntimeException(e);
-        }
-
-        jms.send(message);
+        Map<String, Object> model = new HashMap<>();
+        model.put("code", code);
+        mailSenderService.sendHtmlEmail(email, AUTHORIZE_MAIL_TITLE, "authorize/mail", model);
     }
 
     @Override
