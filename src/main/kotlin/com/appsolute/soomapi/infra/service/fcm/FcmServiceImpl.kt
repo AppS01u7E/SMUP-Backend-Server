e424af07d022559c4bdd63720692ee304be3e428
jinu <jinwoo794533@gmail.com>
2022-01-12 01:44:48 +0900

    :sparkles: FCMService 구체화

--- src/main/kotlin/com/appsolute/soomapi/infra/service/fcm/FcmServiceImpl.kt
+++ src/main/kotlin/com/appsolute/soomapi/infra/service/fcm/FcmServiceImpl.kt
@@ -1,13 +1,18 @@
 package com.appsolute.soomapi.infra.service.fcm
 
-import com.appsolulte.smupbackendserver.domain.account.repository.DeviceTokenRepository
+import com.appsolute.soomapi.domain.account.repository.DeviceTokenRepository
 import com.appsolute.soomapi.infra.env.property.FcmProperty
 import com.appsolute.soomapi.infra.fcm.FcmMessage
 import com.appsolute.soomapi.infra.fcm.Message
-import com.appsolute.soomapi.infra.fcm.Notification
 import com.appsolute.soomapi.infra.exception.DeviceTokenNotFoundException
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.google.auth.oauth2.GoogleCredentials
+import com.google.firebase.FirebaseApp
+import com.google.firebase.FirebaseOptions
+import com.google.firebase.messaging.BatchResponse
+import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.messaging.MulticastMessage
+import com.google.firebase.messaging.Notification
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.Request
@@ -15,6 +20,8 @@ import okhttp3.RequestBody
 import org.springframework.core.io.ClassPathResource
 import org.springframework.http.HttpHeaders
 import org.springframework.stereotype.Service
+import java.util.*
+import javax.annotation.PostConstruct
 
 @Service
 class FcmServiceImpl(
@@ -22,63 +29,61 @@ class FcmServiceImpl(
 ): FcmService {
     private lateinit var deviceTokenRepository: DeviceTokenRepository
     private val objectMapper: ObjectMapper? = null
+    private lateinit var instance: FirebaseMessaging
 
 
-    override fun sendMessageTo(targetId: String, title: String, body: String): List<String> {
-        var tokenList: List<String> = ArrayList<String>()
-        val message: MutableList<String> = ArrayList<String>()
-        try {
-            tokenList = (deviceTokenRepository.findById(targetId).orElse(null)?: throw DeviceTokenNotFoundException()).getToken()
-            tokenList.forEach {
-                    s ->  message.add(makeMessage(s, title, body)!!)
-            }
-        } catch (e: NullPointerException) {
-            throw DeviceTokenNotFoundException()
-        }
-        val client = OkHttpClient()
-        val responseList: MutableList<String> = ArrayList<String>()
-
-        message.forEach{
-                s ->
-            run {
-                val requestBody = (RequestBody.create("application/json; charset=utf-8".toMediaType(), s))
-                val request: Request = Request.Builder()
-                    .url(fcmProperty.url)
-                    .post(requestBody)
-                    .addHeader(HttpHeaders.AUTHORIZATION, "Bearer $accessToken")
-                    .addHeader(HttpHeaders.CONTENT_TYPE, "application/json; UTF-8")
-                    .build()
-                val response = client.newCall(request)
-                    .execute()
-                responseList.add(response.message)
-            }
-        }
-        return responseList
+    override fun sendTargetMessage(targetToken: String, title: String, body: String) {
+        this.sendTargetMessage(targetToken, title, body, null)
     }
 
-
-    override fun sendMessageRangeTo(receiverIds: List<String>, title: String, body: String) {
-        for (i in receiverIds) {
-            sendMessageTo(i, title, body)
-        }
+    override fun sendTargetMessage(targetToken: String, title: String, body: String, image: String?) {
+        val notificatoin: Notification = Notification.builder()
+            .setTitle(title)
+            .setBody(body)
+            .setImage(image)
+            .build()
+        val message = com.google.firebase.messaging.Message.builder()
+            .setToken(targetToken)
+            .setNotification(notificatoin)
+            .build()
+        sendMessage(message)
     }
 
-
-    private fun makeMessage(targetToken: String, title: String, body: String): String? {
-        val fcmMessage = FcmMessage(Message(targetToken, Notification(title, body)))
-
-        return objectMapper!!.writeValueAsString(fcmMessage)
+    override fun sendTopicMessage(topic: String, title: String, body: String) {
+        this.sendTopicMessage(topic, title, body, null)
     }
 
-    private val accessToken: String
-        get() {
-            val firebaseConfigPath = fcmProperty.configPath
-            val googleCredentials = GoogleCredentials
-                .fromStream(ClassPathResource(firebaseConfigPath).inputStream)
-                .createScoped(java.util.List.of("https://www.googleapis.com/auth/cloud-platform"))
-            googleCredentials.refreshIfExpired()
-            return googleCredentials.accessToken.tokenValue
-        }
+    override fun sendTopicMessage(topic: String, title: String, body: String, image: String?) {
+        val notice: Notification = Notification.builder()
+            .setTitle(title)
+            .setBody(body)
+            .setImage(image)
+            .build()
+        val msg = com.google.firebase.messaging.Message.builder()
+            .setToken(topic)
+            .setNotification(notice)
+            .build()
+        sendMessage(msg)
+    }
+
+    override fun sendMessage(message: com.google.firebase.messaging.Message): String {
+        return this.instance.send(message)
+    }
+
+    override fun sendMessage(message: MulticastMessage): BatchResponse {
+        return this.instance.sendMulticast(message)
+    }
+
+    @PostConstruct
+    override fun init() {
+        val googleCredentials: GoogleCredentials = GoogleCredentials.fromStream(ClassPathResource(fcmProperty.configPath).inputStream).createScoped((Arrays.asList(fcmProperty.fireBaseCreateScoped)))
+
+        val secondaryAppConfig: FirebaseOptions = FirebaseOptions.builder()
+            .setCredentials(googleCredentials)
+            .build()
+        val app: FirebaseApp = FirebaseApp.initializeApp(secondaryAppConfig)
+        this.instance = FirebaseMessaging.getInstance(app)
+    }
 
 
 }
