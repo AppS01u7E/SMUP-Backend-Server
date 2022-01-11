cf96749c1f04a290e0e38b8775520c38bc71f1d0
jinu <jinwoo794533@gmail.com>
2022-01-11 14:08:03 +0900

    :zap: FcmProperty 위치 변경

---
+++ src/main/kotlin/com/appsolute/soomapi/global/property/FcmProperty.kt
@@ -0,0 +1,13 @@
+package com.appsolute.soomapi.global.property
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConstructorBinding
+import org.springframework.context.annotation.Configuration
+import java.beans.ConstructorProperties
+
+@ConstructorBinding
+@ConfigurationProperties("fcm")
+data class FcmProperty (
+    val url: String,
+    val configPath: String
+)
