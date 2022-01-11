9b7a2f3b392e10dd761a67d8dfd6811eb97c7015
jinu <jinwoo794533@gmail.com>
2022-01-11 16:32:53 +0900

    :zap: Property 분리

--- src/main/kotlin/com/appsolute/soomapi/infra/env/property/FcmProperty.kt
+++ src/main/kotlin/com/appsolute/soomapi/infra/env/property/FcmProperty.kt
@@ -1,4 +1,4 @@
-package com.appsolute.soomapi.global.env.property
+package com.appsolute.soomapi.infra.env.property
 
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
