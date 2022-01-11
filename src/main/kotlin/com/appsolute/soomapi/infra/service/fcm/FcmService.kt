4e406a543a58513cb6111030e45f273b59fbd27c
jinu <jinwoo794533@gmail.com>
2022-01-12 01:44:35 +0900

    :sparkles: FCMService 추상화

--- src/main/kotlin/com/appsolute/soomapi/infra/service/fcm/FcmService.kt
+++ src/main/kotlin/com/appsolute/soomapi/infra/service/fcm/FcmService.kt
@@ -1,8 +1,24 @@
 package com.appsolute.soomapi.infra.service.fcm
 
+import com.google.firebase.messaging.BatchResponse
+import com.google.firebase.messaging.Message
+import com.google.firebase.messaging.MulticastMessage
+
 
 interface FcmService {
-    fun sendMessageTo(targetId: String, title: String, body: String): List<String>
+    fun sendTargetMessage(targetToken: String, title: String, body: String)
 
-    fun sendMessageRangeTo(receiverIds: List<String>, title: String, body: String)
+    fun sendTargetMessage(targetToken: String, title: String, body: String, image: String?)
+
+    fun sendTopicMessage(topic: String, title: String, body: String)
+
+    fun sendTopicMessage(topic: String, title: String, body: String, image: String?)
+
+    fun sendMessage(message: Message): String
+
+    fun init()
+
+    fun sendMessage(message: MulticastMessage): BatchResponse
+
+
 }
